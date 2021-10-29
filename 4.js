5a8ace98cf3a4bca54b856013fb5d25a09effb59
kdrewremix <93355514+kdrewremix@users.noreply.github.com>
2021-10-28 19:51:43 -0500

    commit

---
+++ 4.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("input");
+const btnWacha = document.querySelector(".btn.whachame");
+
+btnWacha.addEventListener("click", () => {
+
+    if (input.value != "") {
+
+        if (input.value == "culin") {
+            document.location.href = './carta.html'
+
+        } else {
+            alert("Fuera de aqui sapo")
+
+            window.location.href = "https://i2.wp.com/acuariomichin.com/wp-content/uploads/2020/08/sapos-croan-fuerte.jpg?resize=1024%2C640&ssl=1";
+        }
+    }
+    else {
+        alert("Llena porfavor")
+
+    }
+
+
+})
